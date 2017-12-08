3372f17a8559a016602c5a864e47c2310a943ac1
Anton Poleshchuk <anton@poleshchuk.com>
2017-12-08 13:56:37 +0300

    feat: constants from options

--- techs/festxml.js
+++ techs/festxml.js
@@ -11,15 +11,29 @@ module.exports = buildFlow.create()
 	.useSourceFilename('page', '?.page.xml')
 	.useFileList('xml')
     .defineOption('requireNodeSources', {})
+    .defineOption('constants', {})
     .builder(function (page, files) {
         return this.node.requireNodeSources(this._requireNodeSources)
-            .then(function() {
+            .then(() => {
                 let dirname = path.dirname(page);
                 let sources = files.concat({fullname: page}).map(function(file) {
                     let relative = path.relative(dirname, file.fullname);
                     return `\t<fest:include src="${relative}"/>`;
                 }).join('\n');
-                return `<fest:template xmlns:fest="http://fest.mail.ru" context_name="json">\n${sources}\n</fest:template>`;
+                let constants = Object.keys(this._constants).reduce((constants, key) => {
+                    let value = this._constants[key];
+                    if (value.charAt(0) === '?') {
+                        value = this.node.unmaskTargetName(value);
+                    }
+                    return {
+                        ...constants,
+                        [key]: {
+                            src: value
+                        }
+                    }
+                }, {});
+                constants = `<fest:script>(json.CONSTANTS = (json.CONSTANTS || {})).TARGET = ${JSON.stringify(constants, null, 4)};</fest:script>`;
+                return `<fest:template xmlns:fest="http://fest.mail.ru" context_name="json">\n${constants}\n${sources}\n</fest:template>`;
             });
 	})
 	.createTech();
